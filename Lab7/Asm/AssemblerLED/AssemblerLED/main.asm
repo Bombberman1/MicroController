37eb3a919a128b6bea21ab392bb655231c7f151e
Oleg <lezik569@gmail.com>
2024-04-16 17:04:56 +0300

    Working Algorythms

--- Lab7/Asm/AssemblerLED/AssemblerLED/main.asm
+++ Lab7/Asm/AssemblerLED/AssemblerLED/main.asm
@@ -18,42 +18,100 @@ out DDRF, r16
 ldi r16, 0b00001000
 sts DDRK, r16
 
-ldi r17, 0b00000000
-ldi r18, 0b00000000
+ldi r16, 0b00101000
+sts PORTL, r16
 
-ldi r20, 0b11111111
+.def timerReg = r19
+
+.def ledA = r21
+.def ledF = r22
+ldi ledF, 0b10000000
+
+.def counterAH = r23
+ldi counterAH, 0b10000000
+.def counterAL = r24
+ldi counterAL, 0b00000001
+.def counterF = r25
+ldi counterF, 0b10000000
+
 
 start:
-    rjmp ledAswitch
+	lds r16, PINL
+	sbrs r16, 3
+    rcall ledAswitch
+	lds r16, PINL
+	sbrs r16, 5
+	rcall ledFswitch
 	rjmp start
 
 
-ledAswitch: rcall timer1
-	eor r17, r20
-	out PORTA, r17
-	rjmp ledAswitch
+ledAswitch: ; algo3
+	rcall timer4
+	ldi ledA, 0b00000000
+	or ledA, counterAH
+	or ledA, counterAL
+	out PORTA, ledA
+	lsr counterAH
+	lsl counterAL
+	cpi counterAH, 0b00001000
+	brne ledAswitch
+	rjmp resetA
 
-timer1:
+resetA:
+	rcall timer4
+	ldi ledA, 0b00000000
+	out PORTA, ledA
+	ldi counterAH, 0b10000000
+	ldi counterAL, 0b00000001
+	ret
+
+ledFswitch: ; algo6
+	rcall timer4
+	out PORTF, ledF
+	lsr ledF
+	lsr ledF
+	lsr counterF
+	cpi counterF, 0b00001000
+	breq setFcontinue
+	cpi counterF, 0b00000000
+	brne ledFswitch
+	rjmp resetF
+
+setFcontinue:
+	ldi ledF, 0b01000000
+	rjmp ledFswitch
+
+resetF:
+	rcall timer4
+	out PORTF, ledF
+	ldi ledF, 0b10000000
+	ldi counterF, 0b10000000
+	ret
+
+timer4:
 	.equ freq = 58500 ; 0,9 sec
 	// (16/8) * 64 * 10^(-6) * (2^16 - 58500)
-	ldi r19, (freq >> 8)
-	sts TCNT1H, r19
-	ldi r19, (freq & 0x00FF)
-	sts TCNT1L, r19
+	ldi timerReg, (freq >> 8)
+	sts TCNT4H, timerReg
+	ldi timerReg, (freq & 0x00FF)
+	sts TCNT4L, timerReg
 
-	ldi r19, 0b00000000
-	sts TCCR1A, r19
-	ldi r19, 0b00000101 ; OVF 1024
-	sts TCCR1B, r19
+	ldi timerReg, 0b00000000
+	sts TCCR4A, timerReg
+	ldi timerReg, 0b00000101 ; OVF 1024
+	sts TCCR4B, timerReg
+	rcall ovf4
+	ret
 
-ovf1: in r19, TIFR1
-	sbrs r19, TOV1
-	rjmp ovf1
+ovf4:
+	in timerReg, TIFR4
+	sbrs timerReg, TOV4
+	rjmp ovf4
 
-	ldi r19, (1 << TOV1)
-	out TIFR1, r19
+	ldi timerReg, (1 << TOV4)
+	out TIFR4, timerReg
 
-	ldi r19, 0b00000000
-	sts TCCR1B, r19
+	ldi timerReg, 0b00000000
+	sts TCCR4B, timerReg
 	ret
 
